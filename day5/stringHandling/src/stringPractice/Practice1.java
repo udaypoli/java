f38b939ca19665969c2e4dd9464a697c0846a719
uday sagan <udaypoli121@gmail.com>
2024-11-06 22:49:36 +0530

    String program practice1 modified

--- day5/stringHandling/src/stringPractice/Practice1.java
+++ day5/stringHandling/src/stringPractice/Practice1.java
@@ -5,24 +5,50 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice1 {
+	
+	boolean alphabates(char ch){
+		var count=0;
+		if (ch>='A'&&ch<='Z'&&ch>='a'&&ch<='z') {
+			return true;
+		}
+		return false;
+	}
+	
+	boolean numbers(char ch){
+		var count=0;
+		if (ch>='0'&&ch<='9') {
+			return true;
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
-		String s="hello world";
-		String a[]=s.split(" ");
-		ArrayList<StringBuffer> al= new ArrayList<StringBuffer>();
-		StringBuffer sb= new StringBuffer();
-		for (String string : a) {
-			
-			sb.append(string);
-			sb.reverse();
-			al.add(sb);
-			sb.setLength(0);
+		System.out.println("Enter data ");
+		Scanner scan = new Scanner(System.in);
+		String data=scan.nextLine();
+		
+		var alpha=0;
+		var numb=0;
+		var sym=0;
+		var c=' ';
+		for (int i = 0; i < data.length(); i++) {
+			c=data.charAt(i);
+			if (Character.isAlphabetic(c)) {
+				alpha++;
+			}
+			else if (Character.isDigit(c)) {
+				numb++;
+			}
+			else {
+				sym++;
+			}
 		}
-		StringBuffer[] s1=  al.toArray(new StringBuffer[0]);
-		System.out.println(Arrays.toString(s1));
+		System.out.println("alphas : "+alpha);
+		System.out.println("numbs : "+numb);
+		System.out.println("syms : "+sym);
 		
-		
-			
 				
 	}
+
+	
 }
